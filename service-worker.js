f1528aa8258fdd6f01d019332923bfc576bce11c
Zurni75 <Zurn@gmx.de>
2026-09-22 21:28:13 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='nsc-schmiede-shell-v0513';
+const CACHE='nsc-schmiede-shell-v0520';
 const SHELL=['/','/index.html','/manifest.webmanifest','/icon-192.png','/icon-512.png','/header-art.jpg','/theme-fantasy.jpg','/theme-london.jpg','/theme-america.jpg','/theme-horror.jpg','/theme-scifi.jpg','/theme-neutral.jpg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
